e2f54c42e1783602432ed2ee5638973242a332db
inTR4NCE <tanishqsethi760@gmail.com>
2026-06-23 19:12:18 +0530

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,10 @@
+const amount = 1000
+
+if (amount > 500) {
+    console.log("Amount is greater than 500");
+}
+else {
+    console.log("Amount is less than or equal to 500");
+};
+
+console.log("this is my first node js app");
